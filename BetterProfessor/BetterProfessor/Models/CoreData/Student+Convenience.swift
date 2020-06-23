bac3765445857cf3b86f864018f4315b9edf966a
Hunter Oppel <hunter.oppel@gmail.com>
2020-06-23 12:42:17 -0400

    Imported core data and changed storyboard

---
+++ BetterProfessor/BetterProfessor/Models/CoreData/Student+Convenience.swift
@@ -0,0 +1,20 @@
+//
+//  Student+Convenience.swift
+//  BetterProfessor
+//
+//  Created by Stephanie Ballard on 6/22/20.
+//  Copyright © 2020 Stephanie Ballard. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Student {
+    @discardableResult convenience init(name: String,
+                                        id: Int64,
+                                        context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        self.init(context: context)
+        self.name = name
+        self.id = id
+    }
+}
